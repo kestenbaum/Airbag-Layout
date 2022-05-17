31d65b6cec15c837535d982143ab9984ba9a1647
Vovnenko Alexei <kespa71for@gmail.com>
2022-05-17 08:24:56 +0300

    create new project

---
+++ script/index.js
@@ -0,0 +1,14 @@
+//Slider first screen
+   const swiper = new Swiper(('.swiper'), {
+      // Default parameters
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+         el: '.swiper-pagination',
+         type: 'bullets',
+       },
+    })
+
+   
+
+
